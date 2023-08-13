35ef3efc6f588b9502e2cd89ea761648f6f07e69
YAMADA Kazuaki <iterable.company@gmail.com>
2023-08-13 21:30:16 +0900

    10.1

---
+++ Technique/Solve.hs
@@ -0,0 +1,12 @@
+solveRPN :: String -> Double
+solveRPN expr -> head . foldl foldingFunction [] . words
+    where
+        foldingFunction (x:y:ys) "*" = (x * y):ys
+        foldingFunction (x:y:ys) "+" = (x + y):ys
+        foldingFunction (x:y:ys) "-" = (x - y):ys
+        foldingFunction (x:y:ys) "/" = (x / y):ys
+        foldingFunction (x:y:ys) "^" = (y ** x):ys
+        foldingFunction (x:xs) "ln" = log x:xs
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
+
